b9201e8f74b2bf4608f9bbeea22a58267480e7c2
DanielSchn <d.schneider@dsc-mail.de>
2024-10-17 17:19:55 +0200

    add first throttling

--- forum_app/api/permissions.py
+++ forum_app/api/permissions.py
@@ -21,7 +21,6 @@ class CustomQuestionPermission(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        print(f"HAS PERMISSION SECTION: Request User: {request.user}, Is authenticated: {request.user.is_authenticated}, Methode: {request.method}")
         if request.method in permissions.SAFE_METHODS:
             return True
         elif request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
@@ -29,12 +28,9 @@ class CustomQuestionPermission(permissions.BasePermission):
         return False
 
     def has_object_permission(self, request, view, obj):
-        print(f"HAS OBJECT PERMISSION SECTION: Author: {obj.author}, Request User: {request.user}, Is Staff: {request.user.is_staff}, Methode: {request.method}")
         if request.method in permissions.SAFE_METHODS:
-            print('SAFE_METHODS ABSCHNITT')
             return True
         elif request.method in ['PUT', 'PATCH']:
-            print('PUT PATCH ABSCHNITT')
             return obj.author == request.user or request.user.is_staff
         elif request.method == 'DELETE':
             return request.user.is_staff
