eadf00e3cd228d5baa25a4fd493cd38bbb7d629c
DanielSchn <d.schneider@dsc-mail.de>
2024-10-18 15:34:39 +0200

    filtering and ordering

--- forum_app/api/views.py
+++ forum_app/api/views.py
@@ -1,9 +1,11 @@
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics, permissions, filters
 from forum_app.models import Like, Question, Answer
 from .serializers import QuestionSerializer, AnswerSerializer, LikeSerializer
 from .permissions import IsOwnerOrAdmin, CustomQuestionPermission
 from .throttling import QuestionThrottle #, QuestionGetThrottle, QuestionPostThrottle
 from rest_framework.throttling import ScopedRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
@@ -29,6 +31,11 @@ class AnswerListCreateView(generics.ListCreateAPIView):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author__username']
+    search_fields = ['content', '=author__username']
+    ordering_fields = ['content', 'author__username']
+    ordering = ['-content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -52,10 +59,25 @@ class AnswerDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrAdmin]
 
 
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_query_param = 'p'
+
+
+class CustomLimitOffsetPAgination(LimitOffsetPagination):
+    default_limit = 5
+    limit_query_param = 'limit'
+    offset_query_param = 'offset'
+    max_limit = 100
+
+
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
     permission_classes = [IsOwnerOrAdmin]
+    pagination_class = CustomLimitOffsetPAgination
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
